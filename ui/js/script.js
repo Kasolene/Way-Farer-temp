db8f5243ba2c710e0032695916ea7bb1d6040f6d
Nickson Kasolene <nicolaskasolene@gmail.com>
2019-07-12 18:59:21 +0200

    deploy gh-pages

--- ui/js/script.js
+++ ui/js/script.js
@@ -1,4 +1,4 @@
-// LOGIN FORM VALIDATION
+// SIGN UP FORM VALIDATION
 const loginValidation = () => {
    let email = document.getElementById("username").value;
    let password = document.getElementById("password").value;
@@ -12,4 +12,52 @@ const loginValidation = () => {
     }
     
     }
-   
+
+// TRIP MODAL POPUP
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button =>{
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal =>{
+        closeModal(modal)
+    })
+
+})
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+    if(modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if(modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+}
+// DISPLAY ADD NEW TRIP
+const showText = () =>{
+    const tripBtn = document.getElementById('panel');
+    const newTrip =  document.getElementById('newTrip');
+    newTrip.removeAttribute('style');
+    tripBtn.innerHTML = '';
+}
+
+document.querySelector("div#trip")
+.addEventListener('click',showText);
+
